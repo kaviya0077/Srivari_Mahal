c61f58c6cc87a058e3419aa07bd0fca51d7a6619
kaviyavp <kaviyavpcs@gmail.com>
2026-01-01 09:13:55 +0530

    Add Changes in Multiple Files

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # ------------------------------------------------
 # Base Directory
@@ -129,8 +130,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "srivarimahal2025kpm@gmail.com"
-EMAIL_HOST_PASSWORD = "eepw ibge bxej atwk"
+EMAIL_HOST_USER = os.getenv("srivarimahal2025kpm@gmail.com")
+EMAIL_HOST_PASSWORD = os.getenv("eepw ibge bxej atwk")
 DEFAULT_FROM_EMAIL = "srivarimahal2025kpm@gmail.com"
 
 # ------------------------------------------------
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,7 +5,7 @@ const BASE_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
-  timeout: 10000,
+  timeout: 30000,
   headers: { "Content-Type": "application/json" },
 });
 
